075e2eb05546853e52584cb91b887a9e27ba6f12
Zung Nguen <38420456+kanzert@users.noreply.github.com>
2020-04-17 11:15:43 +0300

    add spring boot JPA(postgres) with spring security

---
+++ src/main/java/org/example/springboot/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package org.example.springboot.repos;
+
+import org.example.springboot.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
